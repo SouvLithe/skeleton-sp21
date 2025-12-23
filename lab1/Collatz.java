ff4777dbe6c1e3a4ff8b8be1b880b4d751a2c026
SouvLithe <SouvLithe@gmail.com>
2025-12-23 10:59:14 +0800

    冲突解决3

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -3,10 +3,16 @@
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
-    public static int nextNumber(int n) {
-        return n % 2 == 0 ? n/2 : 3*n + 1;
-    }
+        /** Buggy implementation of nextNumber! */
+        public static int nextNumber(int n) {
+            if (n  == 128) {
+                return 1;
+            } else if (n == 5) {
+                return 3 * n + 1;
+            } else {
+                return n * 2;
+            }
+        }
 
     public static void main(String[] args) {
         int n = 5;
